30e263cb8530a7fd1407a96ac4fb452019765c9e
Chonkar-jay <120003007+Chonkar-jay@users.noreply.github.com>
2023-03-07 17:28:26 +0530

    GIT
    Completed with changing of color dynamically from color-picker during mouse hover to grid.

--- index.js
+++ index.js
@@ -31,11 +31,20 @@ function createGrid(x) {
     createGrid(24);
 
     var gridItems = document.querySelectorAll(".grid");
-    gridItems.forEach(function(item) {
+    color = document.getElementById('pick');
+     //var gridItems = document.getElementsById('pick').value= "#FF8040";
+     gridItems.forEach(function(item) {
+      color = document.getElementById('pick');
       item.addEventListener("mouseover", function() {
-        this.style.backgroundColor = "black";
+      this.style.backgroundColor = color.value;
       });
     });
+    //   const color = document.getElementById('favcolor').value;
+    //   document.querySelectorAll(".row").forEach(function(row) {
+    //   row.addEventListener("mouseover", function() {
+    //     this.style.backgroundColor = color;
+    //   });
+    // });
 
     var newGridButton = document.querySelector(".newGrid");
     newGridButton.addEventListener("click", function() {
@@ -44,7 +53,7 @@ function createGrid(x) {
       var gridItems = document.querySelectorAll(".grid");
       gridItems.forEach(function(item) {
         item.addEventListener("mouseover", function() {
-          this.style.backgroundColor = "black";
+          this.style.backgroundColor = color.value;
         });
       });
     });
@@ -52,10 +61,3 @@ function createGrid(x) {
 
 
 
-  const color = document.getElementById('favcolor').value;
-
-document.querySelectorAll(".row").forEach(function(row) {
-  row.addEventListener("mouseover", function() {
-    this.style.backgroundColor = color;
-  });
-});
